3c181b1694211e0f87be212e1af254475bdfc764
Alistair Campbell <AJCampbell@conestogac.on.ca>
2023-10-22 13:45:55 -0400

    added task list

---
+++ Client/Productiva/src/components/taskList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="border-r-1 border-anti-flash-white w-36 h-full">
+        <table class="w-full">
+            <tbody class="w-full" id="tableBody">
+                <tr class="w-full" v-for="(task) in tasks" v-on:click="$router.push({ path: `/profile/${item.member_name}/home` })">
+                    <td class="w-full text-anti-flash-white hover:bg-Some-light-grey hover:cursor-pointer">
+                        <p>{{ task }}</p>
+                        <p>date</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            tasks: ["task1", "task2", "task3"]
+        }
+    }
+}
+
+</script>
--- Client/Productiva/src/views/main.vue
+++ Client/Productiva/src/views/main.vue
@@ -1,16 +1,21 @@
 <template>
-    <div class="bg-bg-dark w-screen h-screen">
+    <div class="bg-bg-dark w-screen h-screen overflow-hidden">
         <navbar />
+        <div class="h-full">
+            <taskList></taskList>
+        </div>
     </div>
 </template>
 
 <script>
 import navbar from '../components/navbar.vue';
+import taskList from '../components/taskList.vue';
 
 
 export default {
     components: {
-        navbar
+        navbar,
+        taskList
     }
 }   
 </script>
