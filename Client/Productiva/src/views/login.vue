381d869b0c5f621276fd6e42726315e587009c2d
Alistair Campbell <AJCampbell@conestogac.on.ca>
2023-10-22 11:45:14 -0400

    added login page

---
+++ Client/Productiva/src/views/login.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex w-screen h-screen bg-bg-dark">
+        <div class="bg-nearly-white h-96 w-1/2 rounded-md m-auto flex flex-col items-center">
+            <h1 class="font-mainFont font-bold text-4xl mt-5">Login</h1>
+            <form class="flex flex-col items-center">
+                <input type="text" class="m-5" name="" id="">
+                <input type="text" name="" id="">
+                <input type="button" value="Login" class="font-mainFont bg-brand-main rounded-md p-3 font-bold w-48 text-anti-flash-white m-5">
+            </form>
+        </div>
+    </div>
+</template>
--- Client/Productiva/tailwind.config.js
+++ Client/Productiva/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'mainFont': ['"Inter"']
+      },
+      colors: {
+        'bg-dark': '#212026',
+        'anti-flash-white': '#E5EBEA',
+        'nearly-white': '#D4D4D4',
+        'brand-main': '#FF4269',
+        'Some-light-grey': '#5D5D61',
+      }
+
+    },
   },
   plugins: [],
 }
